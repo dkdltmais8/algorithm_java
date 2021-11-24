3c1a7fc1bdd49ae99edd7bb615ea5b78872c8f64
이종은 <dkdltmais9@gmail.com>
2021-11-24 14:04:20 +0900

    Lv2/짝수화홀수(자바)

---
+++ 프로그래머스/Main/src/first/Solve12_짝수와홀수.java
@@ -0,0 +1,21 @@
+package first;
+
+public class Solve12_Â¦¼ö¿ÍÈ¦¼ö {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		solution(2);
+	}
+	public static String solution(int num) {
+        String answer = "";
+        if (num%2==0)
+        {
+            answer = "Even";
+        }
+        else
+        {
+            answer = "Odd";
+        }
+        return answer;
+    }
+}
